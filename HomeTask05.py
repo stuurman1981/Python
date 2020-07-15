968756605f6b8fada379ab657674562cf0999950
stuurman1981 <stuurman1981@gmail.com>
2020-07-16 00:42:07 +0300

    Add files via upload

---
+++ HomeTask05.py
@@ -0,0 +1,20 @@
+# 1. Напишите функцию, которая возвращает список файлов из директории.
+# 2. Напишите декоратор для этой функции, который распечатает все файлы с
+# раcширением .log из найденных
+import os
+def log_reader(func):
+    def wrapper(*args):
+        func(*args)
+        folder = func(*args)
+        for file in folder:
+            if file.endswith(".log"):
+                full_path = f"{str(args[0])}{os.sep}{file}"
+                with open(f"{full_path}", 'r') as log_file:
+                    print(f'Content of {file} is:')
+                    print(log_file.read())
+        return folder
+    return wrapper
+
+@log_reader
+def file_list(path):
+    return os.listdir(path)
